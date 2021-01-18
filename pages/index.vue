6e29294e336e559b6c2e63c234469c4926b26a83
plugo <plugo@americavirtualsa.com>
2021-01-17 21:03:44 -0300

    cambio en el index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        prueba
+        {{message}}
       </h1>
       <div class="links">
         <a
@@ -28,7 +28,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data: function () {
+    return {
+      message:'asdasdas'
+    }
+  }
+}
 </script>
 
 <style>
